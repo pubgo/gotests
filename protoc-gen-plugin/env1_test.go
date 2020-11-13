10647adff0596d34b41664d5197cdbb673012923
白云辉 <baiyh@weipaitang.com>
2020-11-13 16:26:21 +0800

    fix: update

--- protoc-gen-plugin/env1_test.go
+++ protoc-gen-plugin/env1_test.go
@@ -1,4 +1,4 @@
-package tests
+package protoc_gen_plugin
 
 import (
 	"bytes"
@@ -9,26 +9,8 @@ import (
 	descpb "github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"github.com/pubgo/xerror"
 	"io/ioutil"
-	"regexp"
-	"runtime/debug"
 	"testing"
 )
-
-func TestEnv(t *testing.T) {
-	var _envRegexp = xerror.PanicErr(regexp.Compile(`\${(.+)}`)).(*regexp.Regexp)
-
-	fmt.Println(_envRegexp.MatchString(`${app_secret}`))
-	fmt.Println(_envRegexp.FindStringSubmatch(`${app_secret}`))
-	fmt.Println(_envRegexp.MatchString(`${hh||hello}`))
-	fmt.Println(_envRegexp.FindStringSubmatch(`${hh||hello}`))
-
-	var _safeEnvRegexp = xerror.PanicErr(regexp.Compile(`!{(.+)}`)).(*regexp.Regexp)
-	fmt.Println(_safeEnvRegexp.MatchString(`!{app_secret}`))
-	fmt.Println(_safeEnvRegexp.FindStringSubmatch(`!{app_secret}`))
-	fmt.Println(_safeEnvRegexp.MatchString(`!{hh||hello}`))
-	fmt.Println(_safeEnvRegexp.FindStringSubmatch(`!{hh||hello}`))
-	debug.PrintStack()
-}
 
 func GzipDecode(in []byte) ([]byte, error) {
 	reader, err := gzip.NewReader(bytes.NewReader(in))
---
+++ regexp/a_test.go
@@ -0,0 +1,25 @@
+package regexp
+
+import (
+	"fmt"
+	"github.com/pubgo/xerror"
+	"regexp"
+	"runtime/debug"
+	"testing"
+)
+
+func TestEnv(t *testing.T) {
+	var _envRegexp = xerror.PanicErr(regexp.Compile(`\${(.+)}`)).(*regexp.Regexp)
+
+	fmt.Println(_envRegexp.MatchString(`${app_secret}`))
+	fmt.Println(_envRegexp.FindStringSubmatch(`${app_secret}`))
+	fmt.Println(_envRegexp.MatchString(`${hh||hello}`))
+	fmt.Println(_envRegexp.FindStringSubmatch(`${hh||hello}`))
+
+	var _safeEnvRegexp = xerror.PanicErr(regexp.Compile(`!{(.+)}`)).(*regexp.Regexp)
+	fmt.Println(_safeEnvRegexp.MatchString(`!{app_secret}`))
+	fmt.Println(_safeEnvRegexp.FindStringSubmatch(`!{app_secret}`))
+	fmt.Println(_safeEnvRegexp.MatchString(`!{hh||hello}`))
+	fmt.Println(_safeEnvRegexp.FindStringSubmatch(`!{hh||hello}`))
+	debug.PrintStack()
+}
