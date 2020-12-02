7cbebe18644d4c82a2a65278c52e5651d29147e3
白云辉 <kooksee@163.com>
2020-12-02 14:05:49 +0800

    fix: update

--- testmonkey/bytestr/a.go
+++ testmonkey/bytestr/a.go
@@ -1,7 +1,7 @@
 package bytestr
 
 import (
-	"github.com/pubgo/gotests/testmonkey/a1"
+	"github.com/pubgo/gotests/testmonkey/internal/a1"
 	"reflect"
 	"unsafe"
 )
--- testmonkey/main.go
+++ testmonkey/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/pubgo/gotests/testmonkey/a1"
+	"github.com/pubgo/gotests/testmonkey/internal/a1"
 	_ "unsafe"
 )
 
-//go:linkname hello github.com/pubgo/gotests/testmonkey/a1.(*MyStruct).hello
+//go:linkname hello github.com/pubgo/gotests/testmonkey/internal/a1.(*MyStruct).hello
 func hello(t *a1.MyStruct) string
 
 func main() {
