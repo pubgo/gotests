ff35363c2ec97b45c2999011dd9e3a65d2609834
白云辉 <kooksee@163.com>
2020-12-02 14:31:08 +0800

    fix: update

--- testmonkey/main.go
+++ testmonkey/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/pubgo/gotests/testmonkey/internal/a1"
+	_ "google.golang.org/grpc"
+	"net"
 	_ "unsafe"
 )
 
@@ -12,4 +15,11 @@ func hello(t *a1.MyStruct) string
 func main() {
 	b := a1.New("hello", "hello")
 	fmt.Println(hello(b))
+
+	fmt.Println(dial(context.Background(), func(_ context.Context, _ string) (net.Conn, error) {
+		return nil, nil
+	}, ""))
 }
+
+//go:linkname dial google.golang.org/grpc/internal/transport.dial
+func dial(ctx context.Context, fn func(context.Context, string) (net.Conn, error), addr string) (net.Conn, error)
