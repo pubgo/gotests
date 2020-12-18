ba0c8971339695dec4443b506226e85bd8bcdb1b
白云辉 <kooksee@163.com>
2020-12-19 00:46:50 +0800

    fix: update

--- netdx/example/main.go
+++ netdx/example/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "expvar"
 	"github.com/pubgo/gotests/netdx"
 	"net"
 	"net/http"
@@ -9,6 +10,7 @@ import (
 func init() {
 	_ = netdx.ShowTTY()
 	netdx.L4Dx(true)
+	netdx.L7Dx(true)
 }
 
 func main() {
