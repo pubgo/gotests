e87082e24b54f55a143703e9d2f3291bdcd1c439
白云辉 <kooksee@163.com>
2020-12-24 13:30:48 +0800

    fix: update

--- grpc/grpcall/cmd.go
+++ grpc/grpcall/cmd.go
@@ -9,81 +9,73 @@ import (
 	"google.golang.org/grpc/metadata"
 	reflectpb "google.golang.org/grpc/reflection/grpc_reflection_v1alpha"
 	"google.golang.org/grpc/status"
+	"os"
 	"sync"
 	"time"
-
-	"github.com/spf13/cobra"
 )
 
-func GetCmd() *cobra.Command {
-	var args = func(cmd *cobra.Command) *cobra.Command {
-		return cmd
-	}
+// go run main.go localhost:8080 hello.TestApi Version
+func GetCmd() {
+	args := os.Args[1:]
+	fmt.Println(args)
+	cc, err := BlockingDial(context.Background(), args[0])
+	refClient := grpcreflect.NewClient(context.Background(), reflectpb.NewServerReflectionClient(cc))
+	descSource := DescriptorSourceFromServer(context.Background(), refClient)
+	fmt.Println(descSource.ListServices())
+	fmt.Println(descSource.FindSymbol(args[1]))
+	dsc, _ := descSource.FindSymbol(args[1])
+	sd, _ := dsc.(*desc.ServiceDescriptor)
+	mtd := sd.FindMethodByName(args[2])
+	fmt.Println(mtd)
 
-	return args(&cobra.Command{
-		Use: "grpcall",
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println(args)
-			cc, err := BlockingDial(context.Background(), args[0])
-			refClient := grpcreflect.NewClient(context.Background(), reflectpb.NewServerReflectionClient(cc))
-			descSource := DescriptorSourceFromServer(context.Background(), refClient)
-			fmt.Println(descSource.ListServices())
-			fmt.Println(descSource.FindSymbol(args[1]))
-			dsc, _ := descSource.FindSymbol(args[1])
-			sd, _ := dsc.(*desc.ServiceDescriptor)
-			mtd := sd.FindMethodByName(args[2])
-			fmt.Println(mtd)
+	cc1, _ := New()
 
-			cc1, _ := New()
+	ff, err := ParseFormatterByDesc(descSource, true)
+	xerror.Panic(err)
+	SetDefaultEventHandler(descSource, ff)
+	cc1.eventHandler = defaultInEventHooker
 
-			ff, err := ParseFormatterByDesc(descSource, true)
-			xerror.Panic(err)
-			SetDefaultEventHandler(descSource, ff)
-			cc1.eventHandler = defaultInEventHooker
+	var handler = &DefaultEventHandler1{}
+	cc1.invokeCtl = newInvokeHandler(DefaultEventHandler, handler)
 
-			var handler = &DefaultEventHandler1{}
-			cc1.invokeCtl = newInvokeHandler(DefaultEventHandler, handler)
+	res, err := cc1.Call(args[0], args[1], args[2], `{"input":"hello"}`)
+	time.Sleep(time.Millisecond * 10)
+	fmt.Printf("%#v \n", res)
+	fmt.Printf("%s", res.Data)
+	fmt.Printf("%#v \n", err)
 
-			res, err := cc1.Call(args[0], args[1], args[2], `{"input":"hello"}`)
-			time.Sleep(time.Millisecond * 10)
-			fmt.Printf("%#v \n", res)
-			fmt.Printf("%#v \n", err)
+	wg := sync.WaitGroup{}
+	wg.Add(2)
 
-			wg := sync.WaitGroup{}
-			wg.Add(2)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case res.SendChan <- []byte(`{"input":"hello"}`):
+				fmt.Println("send")
+				time.Sleep(time.Second)
 
-			go func() {
-				defer wg.Done()
-				for {
-					select {
-					case res.SendChan <- []byte(`{"input":"hello"}`):
-						fmt.Println("send")
-						time.Sleep(time.Second)
+			case <-res.DoneChan:
+				return
+			}
+		}
+	}()
 
-					case <-res.DoneChan:
-						return
-					}
-				}
-			}()
+	go func() {
+		defer wg.Done()
 
-			go func() {
-				defer wg.Done()
+		for {
+			select {
+			case msg, ok := <-res.ResultChan:
+				fmt.Println("recv data:", msg, ok)
+			case err := <-res.DoneChan:
+				fmt.Println("done chan: ", err)
+				return
+			}
+		}
+	}()
 
-				for {
-					select {
-					case msg, ok := <-res.ResultChan:
-						fmt.Println("recv data:", msg, ok)
-					case err := <-res.DoneChan:
-						fmt.Println("done chan: ", err)
-						return
-					}
-				}
-			}()
-
-			wg.Wait()
-
-		},
-	})
+	wg.Wait()
 }
 
 type DefaultEventHandler1 struct {
